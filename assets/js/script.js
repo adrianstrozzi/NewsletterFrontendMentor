97ba2adc16247df8caccf6df07c20e9685bf3a22
Adrian Strozzi <adrianstrozzi@gmail.com>
2023-06-08 20:18:42 -0600

    Added JS input value to modal and input clean

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@ let mainContainer = document.getElementById("mainContainer")
 let successModal = document.getElementById("successModal")
 let suscribeBtn = document.getElementById("suscribeBtn")
 let dismissModalBtn = document.getElementById("dismissModalBtn")
+let emailInput = document.getElementById("emailInput")
+let storedEmail = document.getElementById("storedEmail")
 
 suscribeButton.onclick = function () {
   if (mainContainer.style.display !== "none") {
     mainContainer.style.display = "none"
     successModal.style.display = "flex"
+    storedEmail.innerHTML = emailInput.value + ". "
   } else {
     mainContainer.style.display !== "flex"
   }
@@ -16,7 +19,9 @@ dismissModalBtn.onclick = function () {
   if (successModal.style.display !== "none") {
     successModal.style.display = "none"
     mainContainer.style.display = "flex"
+    emailInput.value = ""
   } else {
     successModal.style.display = "none"
   }
 }
+
